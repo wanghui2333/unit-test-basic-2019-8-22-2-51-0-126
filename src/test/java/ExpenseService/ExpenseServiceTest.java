eb929609c88c12723290d77cd8f66a12b3a0c307
wanghui <793147654@qq.com>
2019-08-26 16:06:23 +0800

    完成

--- src/test/java/ExpenseService/ExpenseServiceTest.java
+++ src/test/java/ExpenseService/ExpenseServiceTest.java
@@ -6,42 +6,71 @@ import ExpenseService.Project.Project;
 import ExpenseService.Project.ProjectType;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.catchThrowable;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ExpenseServiceTest {
     @Test
     void should_return_internal_expense_type_if_project_is_internal() throws UnexpectedProjectTypeException {
         // given
+        Project project = new Project(ProjectType.INTERNAL, "internal");
+
         // when
+        ExpenseType expenseType = ExpenseService.getExpenseCodeByProjectTypeAndName(project);
         // then
+        assertEquals(ExpenseType.INTERNAL_PROJECT_EXPENSE, expenseType);
     }
 
     @Test
-    void should_return_expense_type_A_if_project_is_external_and_name_is_project_A() throws UnexpectedProjectTypeException {
+    void should_return_expense_type_A_if_project_is_external_and_name_is_project_A()
+            throws UnexpectedProjectTypeException {
         // given
+        Project project = new Project(ProjectType.EXTERNAL, "Project A");
+
         // when
+        ExpenseType expenseType = ExpenseService.getExpenseCodeByProjectTypeAndName(project);
         // then
+
+        assertEquals(ExpenseType.EXPENSE_TYPE_A, expenseType);
     }
 
     @Test
-    void should_return_expense_type_B_if_project_is_external_and_name_is_project_B() throws UnexpectedProjectTypeException {
+    void should_return_expense_type_B_if_project_is_external_and_name_is_project_B()
+            throws UnexpectedProjectTypeException {
         // given
+        Project project = new Project(ProjectType.EXTERNAL, "Project B");
+
         // when
+        ExpenseType expenseType = ExpenseService.getExpenseCodeByProjectTypeAndName(project);
         // then
+
+        assertEquals(ExpenseType.EXPENSE_TYPE_B, expenseType);
     }
 
     @Test
-    void should_return_other_expense_type_if_project_is_external_and_has_other_name() throws UnexpectedProjectTypeException {
+    void should_return_other_expense_type_if_project_is_external_and_has_other_name()
+            throws UnexpectedProjectTypeException {
         // given
+        Project project = new Project(ProjectType.EXTERNAL, "Other Name");
+
         // when
+        ExpenseType expenseType = ExpenseService.getExpenseCodeByProjectTypeAndName(project);
         // then
+
+        assertEquals(ExpenseType.OTHER_EXPENSE, expenseType);
     }
 
     @Test
     void should_throw_unexpected_project_exception_if_project_is_invalid() {
         // given
+
+        Project project = new Project(ProjectType.UNEXPECTED_PROJECT_TYPE, "Other Name");
+
         // when
-        // then
+        UnexpectedProjectTypeException exception = assertThrows(UnexpectedProjectTypeException.class,
+                () -> ExpenseService.getExpenseCodeByProjectTypeAndName(project));
+
+        // Then
+        assertEquals("You enter invalid project type", exception.getMessage());
     }
 }
